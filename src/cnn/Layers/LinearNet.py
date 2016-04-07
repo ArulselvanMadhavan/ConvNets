ca098746fe4a52b208a56a3a405594d5d0f2530f
Arulselvan Madhavan <arulselv@ccs.neu.edu>
2016-04-07 13:53:20 -0400

    Adding base neuron

--- src/cnn/Layers/LinearNet.py
+++ src/cnn/Layers/LinearNet.py
@@ -1,12 +1,47 @@
 from cnn.Layers.BaseLayer import BaseLayer
+from cnn.Layers.BaseNeuron import BaseNeuron
 import numpy as np
 
 
-class LinearNet(BaseLayer):
+class LinearNet(BaseLayer, BaseNeuron):
     def __init__(self, w, b):
         self._fwdcache = None
-        self.W = w
-        self.b = b
+        self._W = w
+        self._b = b
+        self._dw = None
+        self._db = None
+
+    @property
+    def W(self):
+        return self._W
+
+    @W.setter
+    def W(self, newValue):
+        self._W = newValue
+
+    @property
+    def b(self):
+        return self._b
+
+    @b.setter
+    def b(self, newValue):
+        self._b = newValue
+
+    @property
+    def dw(self):
+        return self._dw
+
+    @dw.setter
+    def dw(self, newValue):
+        self._dw = newValue
+
+    @property
+    def db(self):
+        return self._db
+
+    @db.setter
+    def db(self, newValue):
+        self._db = newValue
 
     @property
     def cache(self):
@@ -29,10 +64,10 @@ class LinearNet(BaseLayer):
         D = np.prod(x.shape[1:])
         x_reshaped = np.reshape(x, (N, -1))
         dx = np.dot(dout, w.T)
-        dw = np.dot(x_reshaped.T, dout)
-        db = np.dot(dout.T, np.ones(N))
+        self.dw = np.dot(x_reshaped.T, dout)
+        self.db = np.dot(dout.T, np.ones(N))
         dx = np.reshape(dx, x.shape)
-        return dx, dw, db
+        return dx
 
     @staticmethod
     def generateWeightsAndBias(D, H, weight_scale):
