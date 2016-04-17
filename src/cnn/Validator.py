b4ccdff1d526ad36a6cd5ef4c8a799ef2ce87c93
Arulselvan Madhavan <arulselv@ccs.neu.edu>
2016-04-16 21:51:30 -0400

    A method for randomly generating samples

--- src/cnn/Validator.py
+++ src/cnn/Validator.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-class Validator(object):
 
+class Validator(object):
     @staticmethod
-    def get_accuracy(model, X, y, num_samples=None, fold_size=100):
+    def get_accuracy(model, X, y, sample_size=None, fold_size=100):
         """
         Calculate the accuracy on the given input using Cross validation
         By sampling the given input in batches and averaging the overall accuracy
@@ -11,20 +11,18 @@ class Validator(object):
         :param X: Input Matrix
         :param y: Expected Results
         :param num_samples: Sample
-        :param batch_size:
+        :param fold_size: size of the fold
         :return:
         """
         N = X.shape[0]
-        if num_samples is not None and N > num_samples:
-            mask = np.random.choice(N, num_samples)
-            N = num_samples
-            X = X[mask]
-            y = y[mask]
 
         folds = N // fold_size
         if N % fold_size != 0:
             folds += 1
         y_pred = []
+        if sample_size is not None and N > sample_size:
+            N = sample_size
+            X, y = Validator.get_samples(sample_size, N, X, y)
         for i in range(folds):
             start = i * fold_size
             end = (i + 1) * fold_size
@@ -33,3 +31,18 @@ class Validator(object):
         y_pred = np.hstack(y_pred)
         acc = np.mean(y_pred == y)
         return acc
+
+    @staticmethod
+    def get_samples(sample_size, N, X, y):
+        """
+        Generate random samples from input
+        :param sample_size:
+        :param N:
+        :param X:
+        :param y:
+        :return:
+        """
+        mask = np.random.choice(N, sample_size)
+        X = X[mask]
+        y = y[mask]
+        return X, y
