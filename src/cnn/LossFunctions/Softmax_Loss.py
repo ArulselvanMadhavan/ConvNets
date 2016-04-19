0df85ebac4b2c0b187d340da18443c4e6be24ad2
Arulselvan Madhavan <arulselv@ccs.neu.edu>
2016-04-19 15:10:38 -0400

    Adding the loss function from linear classifiers

--- src/cnn/LossFunctions/Softmax_Loss.py
+++ src/cnn/LossFunctions/Softmax_Loss.py
@@ -1,13 +1,21 @@
 import numpy as np
 from cnn.LossFunctions.AbstractLoss import AbstractLoss
 
+
 class Softmax_Loss(AbstractLoss):
     @staticmethod
-    def compute_loss_and_gradient(x,y):
+    def compute_loss_and_gradient(x, y):
         """
 
         :param x:
         :param y:
         :return:
         """
-        print("Softmax loss function should be placed here")
+        probs = np.exp(x - np.max(x, axis=1, keepdims=True))
+        probs /= np.sum(probs, axis=1, keepdims=True)
+        N = x.shape[0]
+        loss = -np.sum(np.log(probs[np.arange(N), y])) / N
+        dx = probs.copy()
+        dx[np.arange(N), y] -= 1
+        dx /= N
+        return loss, dx
